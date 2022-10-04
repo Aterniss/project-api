23d1c7a2041192de00e528cb7f5cc0d7c723af02
Dominik Lichota <dominik.lichota@devoteam.com>
2022-10-04 14:31:48 +0200

    Added ID property to order_dish model

--- Project-API/Models/OrderDish.cs
+++ Project-API/Models/OrderDish.cs
@@ -5,6 +5,7 @@ namespace Project_API.Models
 {
     public partial class OrderDish
     {
+        public int Id { get; set; }
         public int OrderId { get; set; }
         public int DishId { get; set; }
 
